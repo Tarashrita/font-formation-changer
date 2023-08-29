6d169575c6e92a6aa13db35b709a9992773e76de
Tarashrita Ganguly <106442772+Tarashrita@users.noreply.github.com>
2023-08-29 18:05:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 function setup() {
-    canvas = createCanvas(600,500);
-    canvas.center();
-    
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(782,550);
+    canvas.position(560, 100);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+    };
+}
+
+function draw() {
+    background('#E6E6FA');
+}
+
+function modelLoaded() {
+    console.log("PoseNet has been initialized!");
+}
+
