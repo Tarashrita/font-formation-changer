59c9a5be85095a1970f9bda806e9de40c879525e
Tarashrita Ganguly <106442772+Tarashrita@users.noreply.github.com>
2023-09-05 18:13:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -10,11 +14,18 @@ function setup() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
-    };
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = "+leftWristX+"rightWristX"+rightWristX+"difference"+difference);
+    }
 }
 
 function draw() {
-    background('#E6E6FA');
+    background('#FDF4DC');
+    fill("#9B9EC7")
+    textSize(difference)
+    text("Anu",50,400) 
 }
 
 function modelLoaded() {
